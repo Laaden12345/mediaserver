81aabdbccdbd66c926fe620a7e640e02890731e3
BuildTools <unconfigured@null.spigotmc.org>
2022-08-03 13:43:06 +0300

    Create basic docker express app

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT;
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
